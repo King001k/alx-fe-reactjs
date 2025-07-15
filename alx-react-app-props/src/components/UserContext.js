b81759688ea1c635cc00480381cc6c622715b669
King001k <kchibogu1@gmail.com>
2025-07-15 12:26:56 +0100

    Refactored prop drilling to Context API in alx-react-app-props

---
+++ alx-react-app-props/src/components/UserContext.js
@@ -0,0 +1,6 @@
+import { createContext } from 'react'; // Create a context for user data
+
+const UserContext = createContext(); // This will hold the user info globally
+
+export default UserContext;
+
---
+++ alx-react-app-props/src/components/UserInfo.jsx
@@ -0,0 +1,7 @@
+import UserDetails from './UserDetails'; // Last component in the chain
+
+function UserInfo() {
+  return <UserDetails />; // Also no need to pass props
+}
+
+export default UserInfo;
