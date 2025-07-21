85c6bff6958e0fb6f8b55bef6e6d16c652bcab34
King001k <kchibogu1@gmail.com>
2025-07-21 23:44:25 +0100

    Setup Recipe Sharing App with Zustand

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,15 +2,21 @@ import { create } from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }),
+
   addRecipe: (newRecipe) =>
     set((state) => ({
       recipes: [...state.recipes, newRecipe],
     })),
+
   removeRecipe: (index) =>
     set((state) => ({
       recipes: state.recipes.filter((_, i) => i !== index),
     })),
+
   clearRecipes: () => set({ recipes: [] }),
 }));
 
 export default useRecipeStore;
+
